cfc92a3514fbf967f3ddef566f1285de06ef426c
valeriec <cespedes.valeria@hotmail.com>
2022-11-29 14:54:03 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hw3-Directives';
+  showSecret = false;
+  log: Array<number> = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
